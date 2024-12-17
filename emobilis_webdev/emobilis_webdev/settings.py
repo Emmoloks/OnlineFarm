6abb33495150383796a9aebb77e4ae97e8664fed
Emmoloks <emmanuelkaloki77@gmail.com>
2024-12-17 19:33:39 +0300

    emobilis_webdev

--- emobilis_webdev/emobilis_webdev/settings.py
+++ emobilis_webdev/emobilis_webdev/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b28kjw&2#teyar^b-b#f30dgvuv3sbmql$rmkv7nf@za=8s7qr'
+SECRET_KEY = 'django-insecure-v0ndmv2br^tbvm!n2q-g_*fvva0((+nh)^41)v_j^%h0*hn^n6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
--- emobilis_webdev/emobilis_webdev/urls.py
+++ emobilis_webdev/emobilis_webdev/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from core.views import frontpage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
+    path('admin/', admin.site.urls),
 ]
